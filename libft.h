8a8a2360595fa0a6d314013711a23f79583e3847
kimho <rlaghekd777@naver.com>
2021-05-19 18:21:14 +0900

    header

--- libft.h
+++ libft.h
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkim2 <hkim2@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 17:28:50 by hkim2             #+#    #+#             */
+/*   Updated: 2021/05/19 18:14:54 by hkim2            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-int	ft_strlen(const char *s)
+char	*ft_strdup(const char *str);
+int		ft_strlen(const char *s);
 
 #endif
